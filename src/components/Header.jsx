f0d290b7e80e4209c39f13b30496faac126edbaf
AmmaarSiraj <ammarsirajnomor1pl@gmail.com>
2024-11-21 18:33:16 +0700

    header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,10 @@ import { useNavigate } from "react-router-dom"
 const Header = () => {
   const navigate = useNavigate();
   const [dropdownVisible, setDropdownVisible] = useState(false);
-
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   const toggleDropdown = () => {
     setDropdownVisible(!dropdownVisible);
   };
@@ -22,33 +25,39 @@ const Header = () => {
 
       <nav>
         <ul>
-          <li className="float-in"><a href="#" className="" onClick={()=> navigate('/')}>HOME</a></li>
-          <li className="float-in"><a href="#" className="" onClick={()=> navigate('/market')}>MARKET</a></li>
-          <li className="float-in"><a href="#" className="" onClick={()=> navigate('/konsultasi')}>KONSULTASI</a></li>
+          <li className="float-in"><a href="#" className="" onClick={() => navigate('/')}>HOME</a></li>
+          <li className="float-in"><a href="#" className="" onClick={() => navigate('/market')}>MARKET</a></li>
+          <li className="float-in"><a href="#" className="" onClick={() => navigate('/konsultasi')}>KONSULTASI</a></li>
           <li className="float-in">
             <a onClick={toggleDropdown} className="explore-button">
-              EXPLORE {dropdownVisible ? '▲' : '▼'}
+              <span className="explore-text">EXPLORE</span>
+              <span className="explore-icon">{dropdownVisible ? '▲' : '▼'}</span>
             </a>
             {dropdownVisible && (
               <div className="dropdown-menu">
-                <a href="#" onClick={()=> navigate('/calendar')}>KALENDER</a>
-                <a href="#" onClick={()=> navigate('/petalahan')}>PETA LAHAN</a>
-                <a href="#" onClick={()=> navigate('/community')}>FORUM KOMUNITAS</a>
+                <a href="#" onClick={() => navigate('/calendar')}>KALENDER</a>
+                <a href="#" onClick={() => navigate('/petalahan')}>PETA LAHAN</a>
+                <a href="#" onClick={() => navigate('/community')}>FORUM KOMUNITAS</a>
               </div>
             )}
           </li>
         </ul>
         <div className="icon">
-          <div className="icon-wrapper" >
-            <img src={market} alt="Market Icon" onClick={()=> navigate('/checkout')}/>
-            <span className="badge">1</span>
-          </div>
-          <div className="icon-wrapper">
-            <img src={notification} alt="Notification Icon" />
-            <span className="badge">2</span>
-          </div>
-          <div className="icon-wrapper">
-            <img src={profile} alt="Profile Icon" onClick={()=> navigate('/login')}/>
+          <button className="hamburger" onClick={toggleMenu}>
+            ☰
+          </button>
+          <div className={`icon-container ${isMenuOpen ? 'open' : 'closed'}`}>
+            <div className="icon-wrapper" onClick={()=> navigate('/checkout')}>
+              <img src={market} alt="Market Icon" />
+              <span className="badge">1</span>
+            </div>
+            <div className="icon-wrapper">
+              <img src={notification} alt="Notification Icon" />
+              <span className="badge">2</span>
+            </div>
+            <div className="icon-wrapper" onClick={()=> navigate('/login')}>
+              <img src={profile} alt="Profile Icon" />
+            </div>
           </div>
         </div>
       </nav>
