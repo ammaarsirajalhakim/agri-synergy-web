ef4c1dc66a731c2087acb09c79e6deb7402def1c
iqbalmaulanaputra <iqbalputra090303@gmail.com>
2024-11-21 18:08:32 +0700

    new backend

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -1,10 +1,10 @@
 const mysql2 = require('mysql2');
 
 const db = mysql2.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'agrisynergy'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 
