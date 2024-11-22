71837aa0a6f0fbfb357d766b972948a73439f9f8
iqbalmaulanaputra <iqbalputra090303@gmail.com>
2024-11-22 08:05:32 +0700

    new backend

--- backend/src/routes/kalenderRoutes.js
+++ backend/src/routes/kalenderRoutes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const createKalender = require("../controllers/kalender/create");
 const readKalender = require("../controllers/kalender/read");
 const updateKalender = require("../controllers/kalender/update");
+const deleteKalender = require("../controllers/kalender/delete");
 
 router.post("/kalender", createKalender);
 router.get("/kalender", readKalender);
 router.put("/kalender/:id_kalender", updateKalender);
+router.delete("/kalender/:id_kalender", deleteKalender);
 
 module.exports = router;
