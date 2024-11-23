a6e7cef268546bd6ffaadb1bb4b8ac0fb927eb2c
iqbalmaulanaputra <iqbalputra090303@gmail.com>
2024-11-23 21:10:29 +0700

    localstroge token product backend

--- backend/src/routes/produkRoutes.js
+++ backend/src/routes/produkRoutes.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport"); 
 
 const readProduk = require("../controllers/produk/read");
 const createProduk = require("../controllers/produk/create");
 const updateProduk = require("../controllers/produk/update");
 const deleteProduk = require("../controllers/produk/delete");
 
-router.get("/produk", readProduk);
-router.post("/produk", createProduk);
-router.put("/produk/:id_produk", updateProduk);
-router.delete("/produk/:id_produk", deleteProduk);
+router.get("/produk", passport.authenticate('jwt', { session: false }), readProduk);
+router.post("/produk", passport.authenticate('jwt', { session: false }), createProduk);
+router.put("/produk/:id_produk", passport.authenticate('jwt', { session: false }), updateProduk);
+router.delete("/produk/:id_produk", passport.authenticate('jwt', { session: false }), deleteProduk);
 
 module.exports = router;
