e645f5025a0cd48c32889a6dff02fefd05c3383e
iqbalmaulanaputra <iqbalputra090303@gmail.com>
2024-11-24 21:24:38 +0700

    new backend

--- backend/src/routes/produkRoutes.js
+++ backend/src/routes/produkRoutes.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const router = express.Router();
-const passport = require("passport"); 
-const uploadMiddleware = require("../middlewares/upload-file");
+const passport = require("passport");
+const { uploadMiddleware, staticFileMiddleware, validateStaticFile } = require("../middlewares/upload-file");
 
 const readProduk = require("../controllers/produk/read");
 const createProduk = require("../controllers/produk/create");
 const updateProduk = require("../controllers/produk/update");
 const deleteProduk = require("../controllers/produk/delete");
 
+router.use("/file", validateStaticFile, staticFileMiddleware);
+
+router.use('/file/*', (req, res) => {
+  res.status(404).json({
+    success: false,
+    code: 404,
+    message: "File tidak ditemukan",
+    data: null,
+    timestamp: new Date().toLocaleString("en-US", {
+      timeZone: "Asia/Jakarta"
+    }),
+    errors: null
+  });
+});
+
 router.get("/produk", passport.authenticate('jwt', { session: false }), readProduk);
-router.post("/produk", passport.authenticate('jwt', { session: false }), uploadMiddleware,createProduk);
+router.post("/produk", passport.authenticate('jwt', { session: false }), uploadMiddleware, createProduk);
 router.put("/produk/:id_produk", passport.authenticate('jwt', { session: false }), uploadMiddleware, updateProduk);
 router.delete("/produk/:id_produk", passport.authenticate('jwt', { session: false }), deleteProduk);
+
 
 module.exports = router;
