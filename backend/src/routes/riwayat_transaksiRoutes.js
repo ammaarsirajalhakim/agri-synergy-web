d18f2bbb3d54ae5baa85fe0a243d51424274dbdb
iqbalmaulanaputra <iqbalputra090303@gmail.com>
2024-11-25 00:54:53 +0700

    new backend

--- backend/src/routes/riwayat_transaksiRoutes.js
+++ backend/src/routes/riwayat_transaksiRoutes.js
@@ -7,9 +7,9 @@ const createRiwayatTransaksi = require("../controllers/riwayat_transaksi/create"
 const updateRiwayatTransaksi = require("../controllers/riwayat_transaksi/update");
 const deleteRiwayatTransaksi = require("../controllers/riwayat_transaksi/delete");
 
-router.get("/riwayat-transaksi", readRiwayatTransaksi);
-router.post("/riwayat-transaksi", createRiwayatTransaksi);
-router.put("/riwayat-transaksi/:id_rt", updateRiwayatTransaksi);
-router.delete("/riwayat-transaksi/:id_rt", deleteRiwayatTransaksi);
+router.get("/riwayat-transaksi", passport.authenticate('jwt', { session: false }), readRiwayatTransaksi);
+router.post("/riwayat-transaksi", passport.authenticate('jwt', { session: false }), createRiwayatTransaksi);
+router.put("/riwayat-transaksi/:id_rt", passport.authenticate('jwt', { session: false }), updateRiwayatTransaksi);
+router.delete("/riwayat-transaksi/:id_rt", passport.authenticate('jwt', { session: false }), deleteRiwayatTransaksi);
 
 module.exports = router;
