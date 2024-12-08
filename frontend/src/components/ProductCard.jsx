3098ff19d1d5cd78cda23bb18ccd6b78e4a34b31
iqbalmaulanaputra <iqbalputra090303@gmail.com>
2024-12-08 16:14:55 +0700

    news product

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,13 +1,14 @@
+// src/components/ProductCard.js
 import React from 'react';
 import '../css/homepage.css'
 
 
-const ProductCard = ({ image, title, price, stock, rating }) => {
+const ProductCard = ({ image, title, price, stock }) => {
   return (
     <div className="product-card">
       <img src={image} alt={title} className="product-image" />
       <h3>{title}</h3>
-      <p>{"⭐".repeat(rating)}</p> 
+      <p>⭐⭐⭐⭐⭐</p>
       <p>({stock} stock)</p>
         <p>{`Rp ${Number(price).toLocaleString('id-ID')}. -`}</p>
     </div>
